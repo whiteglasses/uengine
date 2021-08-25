d529d1eb19af1615ba7387293ff803003b20c9e3
Laletin Michail <m.laletin@corp.mail.ru>
2021-08-25 13:52:40 +0300

    add pbr

--- setup.py
+++ setup.py
@@ -1,34 +1,8 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
+from setuptools import setup
 
 setup(
-    name="uengine",
-    version="3.6.13",
-    description="a micro webframework based on flask and pymongo",
-    url="https://github.com/viert/uengine",
-    author="Pavel Vorobyov",
-    author_email="aquavitale@yandex.ru",
-    license="MIT",
-    packages=find_packages(),
-    include_package_data=True,
-    package_data={
-        "uengine": ["*.txt", "*.py"]
-    },
-    install_requires=[
-        "jinja2",
-        "flask",
-        "pymongo",
-        "cachelib",
-        "lazy_object_proxy",
-        "ipython",
-        "pylint",
-        "mongomock",
-        "requests",
-        "mtprof",
-    ],
-    entry_points={
-        "console_scripts": [
-            "uengine=uengine.__main__:main",
-        ]
-    }
+    setup_requires=['pbr'],
+    pbr=True,
 )
